b51affca8e32bebd98616e5b3a13a1b256263673
matias <matias.benedetto@gmail.com>
2018-08-29 13:08:03 -0300

    evito error si un menu que pide el tempalte tag no esta definido

--- revista/templatetags/menus.py
+++ revista/templatetags/menus.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.inclusion_tag('menu.html')
 def get_menu(ubicacion=None):
-    menu = Menu.objects.get(ubicacion=ubicacion)
+    menu = Menu.objects.filter(ubicacion=ubicacion).first()
     print(menu)
     return {'menu': menu}
